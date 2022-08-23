905f148b718fc9c531371bf77d05585aeb1cb378
mateo <matheo.developer@gmail.com>
2022-08-23 16:01:42 -0500

    detail post

--- pages/article/[articleId].tsx
+++ pages/article/[articleId].tsx
@@ -1,13 +1,23 @@
 import {useRouter} from 'next/router';
 import type {NextPage} from 'next';
+import {Box, Container, Typography} from '@mui/material';
+import {Header} from '../../components/Header';
 
 const Article: NextPage = () => {
   const router = useRouter();
   const {articleId} = router.query;
 
-  return(
-    <h1>hello {articleId}</h1>
-  )
+  return (
+    <>
+      <Header title="title article" />
+      <Container maxWidth="md">
+        <Box component="div" sx={{p: 20, height: '100vh'}}>
+          <Typography component="h1">Title</Typography>
+          <Typography>Title</Typography>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default Article;
